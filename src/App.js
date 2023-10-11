11a90bf024d1ecd4f2017011f440c3f2832dd507
Lithish <72008587+lithu-lithish@users.noreply.github.com>
2023-10-11 14:09:12 +0530

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { RouterProvider, useNavigate } from "react-router-dom";
-import routes from "./routes/routes";
-import "./index.css";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={routes} />
+      sdfsdf
     </div>
   );
 }
